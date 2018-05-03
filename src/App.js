3766abc037f042f458b60a7b6f166433ada290d4
Nivrity <nivrityaggarwal23@gmail.com>
2018-05-03 09:53:25 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <hr/>
+        <h1>React Router Demo </h1>
+        <hr/>
+        <ul>
+          <li></li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
